0d318463dce6ddd95f10f4c85189c98780a2645d
radin-reth <radin.reth@gmail.com>
2014-11-27 23:04:26 +0700

    Fix multiple ajax request after scroll event trigger

--- app/themes/sample/assets/javascripts/sample/shared/auto_scrollJS.js
+++ app/themes/sample/assets/javascripts/sample/shared/auto_scrollJS.js
@@ -2,66 +2,61 @@
   $.fn.autoScroll = function(options) {
 
     var settings = $.extend({}, $.fn.autoScroll.defaults, options);
-
     var elem = $("#auto-scroll");
 
     if (elem.length) {
       var lastScrollTop = 0;
-      var i = 0;
-      $(window).scroll(function() {
+      $(document).ready(function () {
 
-        var st = $(this).scrollTop();
-        if (st > lastScrollTop)
-        {
-          elem = $("#auto-scroll");
-          var url = elem.find("[class*='deco'] + span a").attr("href");
-          if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50)
+        var ready = true;
+        $(window).scroll(function() {
+          var st = $(this).scrollTop();
+
+          if (st > lastScrollTop)
           {
-            $.ajax({
-              type: 'GET',
-              url: url,
-              beforeSend: function() {},
-              success: function(data) {
-                var result = $(data).find(".image-wrapper");
-                result.append( $("#auto-scroll") );
+            elem = $("#auto-scroll");
+            var url = elem.find("[class*='deco'] + span a").attr("href");
+            if (ready && url && $(window).scrollTop() > $(document).height() - $(window).height() - 50)
+            {
+              ready = false;
+              $.ajax({
+                type: 'GET',
+                url: url,
+                beforeSend: function() {},
+                success: function(data) {
+                  var result = $(data).find(".image-wrapper");
+                  result.append( $("#auto-scroll") );
 
-                $(".Collage").imagesLoaded(function() {
-                  $(".Collage").removeWhitespace();
-                  $(".Collage").collagePlus({
-                    "targetHeight"        : 200,
-                    "fadeSpeed"           : 5000,
-                    "effect"              : "effect-6",
-                    "direction"           : "vertical",
-                    "allowPartialLastRow" : true
+                  $(".Collage").imagesLoaded(function() {
+                    $(".Collage").removeWhitespace();
+                    $(".Collage").collagePlus({
+                      "targetHeight"        : 200,
+                      "fadeSpeed"           : 5000,
+                      "effect"              : "effect-6",
+                      "direction"           : "vertical",
+                      "allowPartialLastRow" : true
+                    });
                   });
-                });
 
-                console.log(url);
-                console.log(++i);
+                  $("#galleries").append(result);
 
-                $("#galleries").append(result);
-                if ( $("#auto-scroll").find("span[class='deco']:last-child").length ) {
-                  $('#auto-scroll').remove();
-                } else {
-                  $('#auto-scroll').replaceWith($(data).find("#auto-scroll"));
-                }
-              },
-              dataType: "html"
-           });
+                  if ( $("#auto-scroll").find("span[class='deco']:last-child").length ) {
+                    $('#auto-scroll').remove();
+                  } else {
+                    $('#auto-scroll').replaceWith($(data).find("#auto-scroll"));
+                  }
+                },
+                dataType: "html"
+
+              }).always(function(){
+                ready = true;
+              });
+            }
           }
-        }
-
-        lastScrollTop = st;
-
+          lastScrollTop = st;
+        });
       });
     }
-
-
-
-
-
-
-
   };
 
   $.fn.autoScroll.defaults = {
